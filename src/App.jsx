6af83216df6691016ca496227836c7e4bd7391ff
Agustina <agustinamagallanez.gmail.com>
2024-09-17 02:38:10 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <>
+      APP FUNCIONANDO
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
